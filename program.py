8512ce9276c5898e13054d38e3f8bbf2c46dd977
lizzygac <egaczynska@student.agh.edu.pl>
2024-05-27 18:15:17 +0200

    Dodałam funkcję wybierz poziom

--- program.py
+++ program.py
@@ -21,3 +21,31 @@ class MistrzKlawiatury:
                        'pszenżytożercy', 'elektroenergetyczny', 'przedstawicielskie', 'słowiańszczyzna',
                        'wytrzymałościowym', 'paradoksalizm', 'zastanawiający', 'przeanalizowanie']
         }
+        self.poziom = None
+
+def wybierz_poziom(self):
+        print("Wybierz tryb: nauka, wyzwanie")
+        tryb = input().lower()  # wybór trybu
+        os.system('cls')  # czyszczenie konsoli
+
+        if tryb == 'nauka':
+            print("Wybrano tryb nauka!")
+            print("Wybierz poziom trudności: łatwy, średni, trudny")
+            self.poziom = input().lower()
+
+            while self.poziom not in ['debug', 'łatwy', 'średni', 'trudny']:
+                print("Niepoprawny poziom. Spróbuj ponownie.")
+                self.poziom = input().lower()
+
+        elif tryb == 'wyzwanie':
+            print("Wybrano tryb wyzwanie!\n")
+            self.poziom = 'wyzwanie'
+        else:
+            os.system('cls')
+            print("Niepoprawny tryb.")
+            self.wybierz_poziom()
+
+        if self.poziom == 'wyzwanie':
+            print("Tryb wyzwania został wybrany. Musisz napisać 10 haseł.")
+            self.baza_hasel['wyzwanie'] = random.sample(self.baza_hasel['łatwy'] + self.baza_hasel['średni'] + self.baza_hasel['trudny'], 10)
+            self.start_time = time.time()
